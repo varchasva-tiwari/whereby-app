9bce425f2c0c50e096a0d3b33a63a350ac6dad2e
181514912 <ronij.2508@gmail.com>
2020-09-02 06:31:45 +0530

    spring init

---
+++ src/main/java/com/demo/whereby/WherebyApplication.java
@@ -0,0 +1,13 @@
+package com.demo.whereby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WherebyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WherebyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/whereby/WherebyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.whereby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WherebyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
