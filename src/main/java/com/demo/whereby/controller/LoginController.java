f399d2657c9586cbac95d33801ac98546b3e41b3
shubh-msharma <shubhamsharma4539@gmail.com>
2020-09-08 16:40:39 +0530

    mofied

--- src/main/java/com/demo/whereby/controller/LoginController.java
+++ src/main/java/com/demo/whereby/controller/LoginController.java
@@ -27,66 +27,6 @@ public class LoginController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-//    public LoginController() {
-//        users.put("publisher1", new MyUser("publisher1", "pass", OpenViduRole.PUBLISHER));
-//        users.put("publisher2", new MyUser("publisher2", "pass", OpenViduRole.PUBLISHER));
-//        users.put("subscriber", new MyUser("subscriber", "pass", OpenViduRole.SUBSCRIBER));
-//    }
-
-//    @RequestMapping(value = "/")
-//    public String logout(HttpSession httpSession) {
-//        if (checkUserLogged(httpSession)) {
-//            return "redirect:/dashboard";
-//        } else {
-//            httpSession.invalidate();
-//            return "index";
-//        }
-//    }
-
-//    @RequestMapping(value = "/dashboard", method = {RequestMethod.GET, RequestMethod.POST})
-//    public String login(@RequestParam(name = "user", required = false) String user,
-//                        @RequestParam(name = "pass", required = false) String pass, Model model, HttpSession httpSession) {
-//
-//        // Check if the user is already logged in
-//        String userName = (String) httpSession.getAttribute("loggedUser");
-//        if (userName != null) {
-//            // User is already logged. Immediately return dashboard
-//            model.addAttribute("username", userName);
-//            return "dashboard";
-//        }
-//
-//        // User wasn't logged and wants to
-//        if (login(user, pass)) { // Correct user-pass
-//
-//            // Validate session and return OK
-//            // Value stored in HttpSession allows us to identify the user in future requests
-//            httpSession.setAttribute("loggedUser", user);
-//            model.addAttribute("username", user);
-//
-//            // Return dashboard.html template
-//            return "dashboard";
-//
-//        } else { // Wrong user-pass
-//            // Invalidate session and redirect to index.html
-//            httpSession.invalidate();
-//            return "redirect:/";
-//        }
-//    }
-
-//    @RequestMapping(value = "/logout", method = RequestMethod.POST)
-//    public String logout(Model model, HttpSession httpSession) {
-//        httpSession.invalidate();
-//        return "redirect:/";
-//    }
-//
-//    private boolean login(String user, String pass) {
-//        return (user != null && pass != null && users.containsKey(user) && users.get(user).pass.equals(pass));
-//    }
-//
-//    private boolean checkUserLogged(HttpSession httpSession) {
-//        return !(httpSession == null || httpSession.getAttribute("loggedUser") == null);
-//    }
-
     @RequestMapping(value = "/registerUser",method = RequestMethod.POST)
     public String userRegistrationHandler(@ModelAttribute RegistrationModel registrationModel) {
         User user = new User();
