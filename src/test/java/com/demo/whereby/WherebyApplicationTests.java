c4ea5918a36ca02fc25929d679cdad029aa3e948
181514912 <ronij.2508@gmail.com>
2020-09-11 20:55:19 +0530

    added cancel buttons

--- src/test/java/com/demo/whereby/WherebyApplicationTests.java
+++ src/test/java/com/demo/whereby/WherebyApplicationTests.java
@@ -1,13 +1,37 @@
 package com.demo.whereby;
 
-import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-class WherebyApplicationTests {
+class WherebyApplicationTests extends AbstractTestNGSpringContextTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeClass
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void testUserDashboard() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .get("/user-dashboard")
+                .accept(MediaType.ALL)
+        ).andExpect(status().isOk());
+    }
 
 }
