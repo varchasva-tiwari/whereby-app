e71d6afa5db583d55a9d5b36f8df3806b401fc9d
shubh-msharma <shubhamsharma4539@gmail.com>
2020-09-12 15:03:41 +0530

    audio fix

---
+++ src/test/java/com/demo/whereby/controllerTest/LoginControllerTest.java
@@ -0,0 +1,24 @@
+package com.demo.whereby.controllerTest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @Test
+    void userRegistrationHandler() {
+    }
+
+    @Test
+    void loginHandler() {
+    }
+
+    @Test
+    void registrationPage() {
+    }
+
+    @Test
+    void homePageHandler() {
+    }
+}
